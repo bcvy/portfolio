4a1a839a81ab9456951b5ff3fef910c634c0af92
bcvy <manka.maciej@gmail.com>
2025-07-18 22:58:58 +0200

    Add files via upload

--- js/swiper-init.js
+++ js/swiper-init.js
@@ -24,3 +24,29 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.swiper-button-prev',
   },
 });
+const swiper2 = new Swiper('.swiper2', {
+  effect: 'fade',
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: false,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
